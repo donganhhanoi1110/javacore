9c1739541a72eb725193bcae8940925f5d58457d
Minh Nguyen <donganhhanoi1110@gmail.com>
2018-07-06 23:23:25 +0700

    MM - Two Dimension Array

--- test/src/test/StaticInstanceOrder.java
+++ test/src/test/StaticInstanceOrder.java
@@ -1,7 +1,16 @@
 package test;
-
+/**
+ * The order should be.
+ * 
+ * 1. static variable
+ * 2. static block
+ * 3. block code
+ * 4. Constructor
+ * @author Minh Nguyen
+ *
+ */
 class StaticInstanceOrder{
-
+        
 		   static String s1 = sM1("a");
 		   {
 		      s1 = sM1("b");
