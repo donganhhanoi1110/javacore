b2777f72a12c18744d943972f0af9da14d2ef4d7
Minh Nguyen <donganhhanoi1110@gmail.com>
2018-07-26 23:28:55 +0700

    MM - Anonymous Class parrt 2

--- test/src/java8/AnonymousClass.java
+++ test/src/java8/AnonymousClass.java
@@ -10,25 +10,35 @@ public class AnonymousClass {
 	public static void main(String[] args) {
 		//#1 Create Anonymous class by abstract class
 		AnonymousInnerOfClass innerClass = new AnonymousInnerOfClass() {
+			//
+			String message = "Print Me";
 			
+			//Instance initializer block
+			{
+				System.out.println("#1 Anonymous Inner Of Class - Instance Initializer Block.");
+			}
 			@Override
 			public void printMessageClass() {
+				
 				System.out.println("#1 Anonymous Inner Of Class");
+				printMe(message);
+				MyClass myClass = new MyClass();
+				myClass.printMyLocalClass();
+			}
+			// Method
+			void printMe(String str) {
+				System.out.println("#1 Anonymous Inner Of Class - " + str);
+			}
+			// Local 
+			class MyClass{
+				void printMyLocalClass() {
+					System.out.println("#1 Anonymous Inner Of Class - Print My Local Class");
+				}
 			}
 		};
+		
 		//Run innerClass
 		innerClass.printMessageClass();
-		
-		//#2 Create Anonymous class by interface
-		AnonymousInnerOfInterface innerInterface = new AnonymousInnerOfInterface() {
-			@Override
-			public void printMessageInterface() {
-				System.out.println("#2 Anonymous Inner Of Interface");
-			}
-		};
-		
-		//Run innerInterface
-		innerInterface.printMessageInterface();
 	}
 }
 
