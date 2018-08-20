c93a54c8616c7496f1d3795762676b6901a794b3
Minh Nguyen <donganhhanoi1110@gmail.com>
2018-08-20 23:45:50 +0700

    MM - Java 8 - Interface Inheritance

---
+++ test/src/java8/defaultmethods/Interface1.java
@@ -0,0 +1,15 @@
+package java8.defaultmethods;
+
+/**
+ * 
+ * @author Minh Nguyen
+ * @2018 https://java.hardibee.com @Copyright
+ *
+ */
+public interface Interface1 {
+	
+	// default method
+	default void hello() {
+		System.out.println("Hello From Interface1");
+	}
+}
---
+++ test/src/java8/defaultmethods/MyClass.java
@@ -0,0 +1,16 @@
+package java8.defaultmethods;
+/**
+ * 
+ * @author Minh Nguyen
+ * @2018 https://java.hardibee.com @Copyright
+ * 
+ */
+public class MyClass 
+	implements Interface1, Interface2{
+	
+	public static void main(String[] args) {
+	}
+
+}
+
+
