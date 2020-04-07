3c2225b9d5df38b897be42b4dbf725af3255df1e
Minh Nguyen <donganhhanoi1110@gmail.com>
2020-04-08 00:26:19 +0700

    WINDOW SLIDING

--- test/src/com/leetcode/MaximumSumOfKConsecutiveElements.java
+++ test/src/com/leetcode/MaximumSumOfKConsecutiveElements.java
@@ -1,4 +1,4 @@
-package com.algorithm;
+package com.leetcode;
 
 import java.util.Scanner;
 
@@ -37,7 +37,7 @@ public class MaximumSumOfKConsecutiveElements {
 
         int maxSum = 0;
         for (int i = 0; i <= nums.length - k; i++) {
-            int currentSum = 0
+            int currentSum = 0;
            for (int j = 0; j < k; j++) {
                currentSum += nums[i + j];
            }
@@ -49,11 +49,60 @@ public class MaximumSumOfKConsecutiveElements {
 
     }
 
+
+    /**
+     * Window Sliding
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int  maxSum2(int[] nums, int k) {
+
+        int window = 0;
+        //first window
+        for (int i = 0; i < k; i++) {
+            window += nums[i];
+        }
+
+        int maxSum = window;
+
+        // Compute sums of remaining windows by
+        // removing first element of previous
+        // window and adding last element of
+        // current window.
+        for (int i = k; i < nums.length; i++) {
+            window = window + nums[i] - nums[i-k];
+            if (window > maxSum) {
+                maxSum = window;
+            }
+        }
+        return maxSum;
+    }
+
+    static int largest(int arr[])
+    {
+        int i;
+
+        // Initialize maximum element
+        int max = arr[0];
+
+        // Traverse array elements from second and
+        // compare every element with current max
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+
+        return max;
+    }
+
+
     public static void main (String[] args) {
         int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
         int k = 4;
         int n = arr.length;
-        System.out.println(maxSum1(arr, n, k));
+        System.out.println(maxSum1(arr, k));
+        System.out.println(maxSum2(arr, k));
     }
 
 
