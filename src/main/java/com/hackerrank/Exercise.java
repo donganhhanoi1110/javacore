efb9ecb2d56233a3b5f214357a5b244a71606673
minhnguyen <donganhhanoi1110>
2020-04-18 12:56:55 +0700

    Change directory

---
+++ src/main/java/com/hackerrank/Exercise.java
@@ -0,0 +1,13 @@
+package com.hackerrank;
+
+public class Exercise {
+
+    public static void exercise1(String[] jewel, String[] stone) {
+    }
+
+    public static void main (String[] args) {
+
+    }
+
+
+}
